0513d37b301c4dcff840fdb980f3be081648de53
Ken Erwin <ken.erwin88@gmail.com>
2016-08-18 13:50:53 -0400

    Added fix for issue 28 -- accept invalid certs
    I believe the solution is to add:
    
    # Added for dealing with invalid certificates.
    add-type -TypeDefinition  @"
    using System.Net;
    using System.Security.Cryptography.X509Certificates;
    public class TrustAllCertsPolicy : ICertificatePolicy {
    public bool CheckValidationResult(
    ServicePoint srvPoint, X509Certificate certificate,
    WebRequest request, int certificateProblem) {
    return true;
    }
    }
    "@
    [System.Net.ServicePointManager]::CertificatePolicy = New-Object
    TrustAllCertsPolicy
    
    to the http.ps1 file.

--- Public/Http.ps1
+++ Public/Http.ps1
@@ -33,7 +33,18 @@ function Http {
         [Parameter(Mandatory, Position=3)]
         [scriptblock]$Should
     )    
-    
+    add-type -TypeDefinition  @"
+        using System.Net;
+        using System.Security.Cryptography.X509Certificates;
+        public class TrustAllCertsPolicy : ICertificatePolicy {
+            public bool CheckValidationResult(
+                ServicePoint srvPoint, X509Certificate certificate,
+                WebRequest request, int certificateProblem) {
+                return true;
+            }
+        }
+"@
+    [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
     $params = Get-PoshspecParam -TestName Http -TestExpression {Invoke-WebRequest -Uri '$Target' -UseBasicParsing -ErrorAction SilentlyContinue} @PSBoundParameters
     
     Invoke-PoshspecExpression @params
