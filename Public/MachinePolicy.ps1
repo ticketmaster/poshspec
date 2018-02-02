9d50d66c8896c7cff1095591a5d72c2b9f6a651b
Stefan Henseler <stefan.henseler@synax.ch>
2018-02-02 08:49:54 +0100

    Adds Machine Policy Function

---
+++ Public/MachinePolicy.ps1
@@ -0,0 +1,36 @@
+<#
+.SYNOPSIS
+    Test if a GroupPolicy is present on the client
+.DESCRIPTION
+    Test if a GroupPolicy is present on the client
+.PARAMETER Target
+    the group policy name to be checked.
+.PARAMETER Should 
+    A Script Block defining a Pester Assertion.  
+.EXAMPLE
+    GroupPolicy 'VeryImportantGPO-1' { should not BeNullOrEmpty }    
+.NOTES
+    Assertions: BeNullOrEmpty
+#>
+    
+function GroupPolicy {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position=1)]
+        [Alias('Name')]
+        [string]$Target,
+        
+        [Parameter(Mandatory, Position=2)]
+        [scriptblock]$Should
+    )
+
+    Process {
+
+        $expression = { Get-CimInstance -ClassName RSOP_GPO -Namespace root/RSOP/Computer -Filter 'Name = "$Target" and Enabled = true and accessDenied = false' }
+        
+        $params = Get-PoshspecParam -TestName GroupPolicy -TestExpression $expression @PSBoundParameters
+        
+        Invoke-PoshspecExpression @params
+    
+    }
+}
