bcb172001fe5b649c2d6c170ee9fb32a3c4ca7dd
Mark Wragg <mark.wragg@elysianit.com>
2019-06-18 16:54:47 +0100

    - Updated PoshSpecDemo to use new Pester syntax - Code formatting/help added to Private functions

--- Private/Get-PoshspecParam.ps1
+++ Private/Get-PoshspecParam.ps1
@@ -1,33 +1,49 @@
+<#
+.SYNOPSIS
+    Returns an object which is used with Invoke-PoshspecExpression to execute a Pester 'It' block with the generated name and test expression values.
+.EXAMPLE
+    Get-PoshspecParam -TestName File -TestExpression {'C:\Temp'} -Target 'C:\Temp' -Should { Should -Exist }
+
+    Name       : File 'C:\Temp' Should -Exist
+    Expression : 'C:\Temp' | Should -Exist
+#>
 function Get-PoshspecParam {
-    [CmdletBinding(DefaultParameterSetName="Default")]
+    [CmdletBinding(DefaultParameterSetName = "Default")]
     param(
-        [Parameter(Mandatory=$true, ParameterSetName="Default")]
-        [Parameter(Mandatory=$true, ParameterSetName="PropertyExpression")]
+        [Parameter(Mandatory, ParameterSetName = "Default")]
+        [Parameter(Mandatory, ParameterSetName = "PropertyExpression")]
         [string]
         $TestName,
-        [Parameter(Mandatory=$true, ParameterSetName="Default")]
-        [Parameter(Mandatory=$true, ParameterSetName="PropertyExpression")]
+
+        [Parameter(Mandatory, ParameterSetName = "Default")]
+        [Parameter(Mandatory, ParameterSetName = "PropertyExpression")]
         [string]
         $TestExpression,
-        [Parameter(Mandatory=$true, ParameterSetName="Default")]
-        [Parameter(Mandatory=$true, ParameterSetName="PropertyExpression")]
+
+        [Parameter(Mandatory, ParameterSetName = "Default")]
+        [Parameter(Mandatory, ParameterSetName = "PropertyExpression")]
         [string]
         $Target,
-        [Parameter(ParameterSetName="Default")]
+
+        [Parameter(ParameterSetName = "Default")]
         [string]
         $FriendlyName,
-        [Parameter(ParameterSetName="Default")]
+
+        [Parameter(ParameterSetName = "Default")]
         [string]
         $Property,
-        [Parameter(Mandatory=$true, ParameterSetName="PropertyExpression")]
+
+        [Parameter(Mandatory, ParameterSetName = "PropertyExpression")]
         [string]
         $PropertyExpression,
-        [Parameter(ParameterSetName="Default")]
-        [Parameter(ParameterSetName="PropertyExpression")]
+
+        [Parameter(ParameterSetName = "Default")]
+        [Parameter(ParameterSetName = "PropertyExpression")]
         [string]
         $Qualifier,
-        [Parameter(Mandatory=$true, ParameterSetName="Default")]
-        [Parameter(Mandatory=$true, ParameterSetName="PropertyExpression")]
+
+        [Parameter(Mandatory, ParameterSetName = "Default")]
+        [Parameter(Mandatory, ParameterSetName = "PropertyExpression")]
         [scriptblock]
         $Should
     )
@@ -35,50 +51,51 @@ function Get-PoshspecParam {
     $assertion = $Should.ToString().Trim()
     $expressionString = $TestExpression.ToString().Trim()
     $PropertyExpression = $PropertyExpression.ToString().Trim()
-    if ($PSBoundParameters.ContainsKey("PropertyExpression"))
-    {
-      $expressionString = $ExecutionContext.InvokeCommand.ExpandString($expressionString)
-      $expressionString = Expand-PoshspecTestExpression $expressionString $PropertyExpression
-      if ($PropertyExpression -like '*.*') {
-        $lastIndexOfPeriod = $PropertyExpression.LastIndexOf('.')
-        $Qualifier = $PropertyExpression.substring(0, $lastIndexOfPeriod)
-        $NewProperty = $PropertyExpression.substring($lastIndexOfPeriod + 1)
-        $nameString = "{0} property '{1}' for '{2}' at '{3}' {4}" -f $TestName, $NewProperty, $Target, $Qualifier, $assertion
-      }
-      else {
-        $nameString = "{0} property '{1}' for '{2}' {3}" -f $TestName, $PropertyExpression, $Target, $assertion
-      }
-      $expressionString += " | $assertion"
-      Write-Output -InputObject ([PSCustomObject]@{Name = $nameString; Expression = $expressionString})
+
+    if ($PSBoundParameters.ContainsKey("PropertyExpression")) {
+        $expressionString = $ExecutionContext.InvokeCommand.ExpandString($expressionString)
+        $expressionString = Expand-PoshspecTestExpression $expressionString $PropertyExpression
+
+        if ($PropertyExpression -like '*.*') {
+            $lastIndexOfPeriod = $PropertyExpression.LastIndexOf('.')
+            $Qualifier = $PropertyExpression.substring(0, $lastIndexOfPeriod)
+            $NewProperty = $PropertyExpression.substring($lastIndexOfPeriod + 1)
+            $nameString = "{0} property '{1}' for '{2}' at '{3}' {4}" -f $TestName, $NewProperty, $Target, $Qualifier, $assertion
+        }
+        else {
+            $nameString = "{0} property '{1}' for '{2}' {3}" -f $TestName, $PropertyExpression, $Target, $assertion
+        }
+
+        $expressionString += " | $assertion"
+        Write-Output -InputObject ([PSCustomObject]@{Name = $nameString; Expression = $expressionString})
     }
-    else
-    {
-      if (-not $PSBoundParameters.ContainsKey("FriendlyName"))
-      {
-          $FriendlyName = $Target
-      }
+    else {
+        if (-not $PSBoundParameters.ContainsKey("FriendlyName")) {
+            $FriendlyName = $Target
+        }
 
-      $expressionString = $TestExpression.ToString().Trim()
+        $expressionString = $TestExpression.ToString().Trim()
 
-      if ($PSBoundParameters.ContainsKey("Property"))
-      {
-          $expressionString += " | Select-Object -ExpandProperty '$Property'"
+        if ($PSBoundParameters.ContainsKey("Property")) {
+            $expressionString += " | Select-Object -ExpandProperty '$Property'"
 
-          if ($PSBoundParameters.ContainsKey("Qualifier"))
-          {
-              $nameString = "{0} property '{1}' for '{2}' at '{3}' {4}" -f $TestName,$Property, $FriendlyName, $Qualifier, $assertion
-          }
-          else
-          {
-              $nameString = "{0} property '{1}' for '{2}' {3}" -f $TestName, $Property, $FriendlyName, $assertion
-          }
-      }
-      else
-      {
-          $nameString = "{0} '{1}' {2}" -f $TestName, $FriendlyName, $assertion
-      }
-      $expressionString += " | $assertion"
-      $expressionString = $ExecutionContext.InvokeCommand.ExpandString($expressionString)
-      Write-Output -InputObject ([PSCustomObject]@{Name = $nameString; Expression = $expressionString})
+            if ($PSBoundParameters.ContainsKey("Qualifier")) {
+                $nameString = "{0} property '{1}' for '{2}' at '{3}' {4}" -f $TestName, $Property, $FriendlyName, $Qualifier, $assertion
+            }
+            else {
+                $nameString = "{0} property '{1}' for '{2}' {3}" -f $TestName, $Property, $FriendlyName, $assertion
+            }
+        }
+        else {
+            $nameString = "{0} '{1}' {2}" -f $TestName, $FriendlyName, $assertion
+        }
+
+        $expressionString += " | $assertion"
+        $expressionString = $ExecutionContext.InvokeCommand.ExpandString($expressionString)
+
+        Write-Output -InputObject ([PSCustomObject]@{
+                Name       = $nameString
+                Expression = $expressionString
+            })
     }
 }
--- poshspecdemo.ps1
+++ poshspecdemo.ps1
@@ -1,47 +1,49 @@
 #Requires -Module Poshspec
 
 Describe 'Services' {
-    Service w32time Status { Should Be Running }
-    Service bits Status { Should Be Stopped }
+    Service w32time Status { Should -Be Running }
+    Service bits Status { Should -Be Stopped }
 }
 
 Describe 'Files' {
-    File C:\inetpub\wwwroot\iisstart.htm { Should Exist }
-    File C:\inetpub\wwwroot\iisstart.htm { Should Contain 'text-align:center' }
+    File C:\inetpub\wwwroot\iisstart.htm { Should -Exist }
+    File C:\inetpub\wwwroot\iisstart.htm { Should -FileContentMatch 'text-align:center' }
 }
 
 Describe 'Registry' {
-    Registry HKLM:\SOFTWARE\Microsoft\Rpc\ClientProtocols { Should Exist }
-    Registry HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\ "SyncDomainWithMembership" { Should Be 1  }
-    Registry 'HKLM:\SOFTWARE\Callahan Auto\' { Should Not Exist }
+    Registry HKLM:\SOFTWARE\Microsoft\Rpc\ClientProtocols { Should -Exist }
+    Registry HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\ "SyncDomainWithMembership" { Should -Be 1 }
+    Registry 'HKLM:\SOFTWARE\Callahan Auto\' { Should -Not -Exist }
 }
 
 Describe 'Http' {
-    TcpPort localhost 80 PingSucceeded { Should Be $true }
-    TcpPort localhost 80 TcpTestSucceeded { Should Be $true }
-    Http http://localhost StatusCode { Should Be 200 }
-    Http http://localhost RawContent { Should Match 'X-Powered-By: ASP.NET' }
-    Http http://localhost RawContent { Should Not Match 'X-Powered-By: Cobal' }
+    TcpPort localhost 80 PingSucceeded { Should -Be $true }
+    TcpPort localhost 80 TcpTestSucceeded { Should -Be $true }
+    Http http://localhost StatusCode { Should -Be 200 }
+    Http http://localhost RawContent { Should -Match 'X-Powered-By: ASP.NET' }
+    Http http://localhost RawContent { Should -Not -Match 'X-Powered-By: Cobal' }
 }
+
 Describe 'Hotfix' {
-    Hotfix KB3116900 { Should Not BeNullOrEmpty}
-    Hotfix KB1112233 { Should BeNullOrEmpty}
+    Hotfix KB3116900 { Should -Not -BeNullOrEmpty }
+    Hotfix KB1112233 { Should -BeNullOrEmpty }
 }
+
 Describe 'CimObject' {
-    CimObject Win32_OperatingSystem SystemDirectory { Should Be C:\WINDOWS\system32 }
-    CimObject root/StandardCimv2/MSFT_NetOffloadGlobalSetting ReceiveSideScaling { Should Be Enabled }
+    CimObject Win32_OperatingSystem SystemDirectory { Should -Be C:\WINDOWS\system32 }
+    CimObject root/StandardCimv2/MSFT_NetOffloadGlobalSetting ReceiveSideScaling { Should -Be Enabled }
 }
 
 
 Describe 'Firewall' {
-   Firewall putty.exe Enabled { Should be "$True" }
-   Firewall putty.exe Action { Should be 'Allow' }
-   Firewall putty.exe Profile { Should be 'Private' }
+    Firewall putty.exe Enabled { Should -Be "$True" }
+    Firewall putty.exe Action { Should -Be 'Allow' }
+    Firewall putty.exe Profile { Should -Be 'Private' }
 }
 
 Describe 'Volume' {
-    Volume C DriveType { should be 'fixed' }
-    Volume C FileSystem { should be 'NTFS' }
-    Volume 'ShouldNotExist' { should BeNullOrEmpty}
+    Volume C DriveType { Should -Be 'fixed' }
+    Volume C FileSystem { Should -Be 'NTFS' }
+    Volume 'ShouldNotExist' { Should -BeNullOrEmpty }
 }
 
